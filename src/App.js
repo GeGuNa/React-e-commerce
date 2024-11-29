66cd25cf24b679fa35f5b72b54e5147e4cfb402e
Nukri <bnukri291@gmail.com>
2024-11-30 03:51:04 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { Kmain, Kmain2 } from './components/main.js' 
+import { Routes, Route, Link} from 'react-router';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<>
+    <Routes>
+		<Route index path="/" element={<Kmain />}/>
+		<Route index path="/ind2" element={<Kmain2 />}/>
+    </Routes>
+  </>);
 }
 
 export default App;
---
+++ src/components/main.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export function Kmain(){ 
+	return (<>
+	
+	<Link to="/ind2">ind2 </Link>
+	
+		<br/>	Kmain <br/>
+	
+	</>)
+}
+
+
+
+export function Kmain2(){ 
+	return (<>
+	
+		<br/>	Kmain2 <br/>
+	<Link to="/"> Home </Link>
+	</>)
+}
